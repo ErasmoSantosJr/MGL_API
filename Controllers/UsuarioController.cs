5543eff755752542ef7f5b8fe3c952dfe737b85e
JohnSilva98 <53828298+JohnSilva98@users.noreply.github.com>
2021-04-27 20:20:20 -0300

    Revert "Exibir game"

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-//Controle Usuario
+
 namespace MGL_API.Controllers
 {
     [Route("api/[controller]")]
